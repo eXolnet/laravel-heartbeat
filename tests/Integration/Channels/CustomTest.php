e1cccabd0cb56df620aae032f19681535912f5c0
Simon Gaudreau <sgaudreau@exolnet.com>
2022-10-03 15:24:25 -0400

    style: fix pint linting errors

--- tests/Integration/Channels/CustomTest.php
+++ tests/Integration/Channels/CustomTest.php
@@ -4,7 +4,6 @@ namespace Exolnet\Heartbeat\Tests\Integration;
 
 use Exolnet\Heartbeat\HeartbeatFacade as Heartbeat;
 use Exolnet\Heartbeat\Tests\Mocks\CustomChannel;
-use Illuminate\Foundation\Application;
 use Mockery as m;
 
 class CustomTest extends TestCase
